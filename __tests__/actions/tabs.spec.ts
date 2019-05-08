maidsafe/sn_browser

fix(tabs): Generate Unique Ids for individual tabs

__tests__/actions/tabs.spec.ts
@@ -1,52 +1,47 @@
 import * as actions from '$Actions/tabs_actions';
 
-describe( 'tab actions', () => {
-    it( 'should have types', () => {
+describe( 'tab actions', () =>
+{
+    it( 'should have types', () =>
+    {
         expect( actions.TYPES ).toBeDefined();
     } );
 
-    it( 'should create an action to add a tab', () => {
-        const payload = { url: 'hi' };
+    it( 'should create an action to add a tab', () =>
+    {
+        const payload = { url: 'hi', tabId: Math.random().toString( 36 ) };
         const expectedAction = {
-            type: actions.TYPES.ADD_TAB,
+            type : actions.TYPES.ADD_TAB,
             payload
         };
         expect( actions.addTab( payload ) ).toEqual( expectedAction );
     } );
-
-    it( 'should create an action to setActiveTab', () => {
-        const payload = { url: 'hi' };
+    it( 'should create an action to updateTab', () =>
+    {
+        const payload = { url: 'hi', tabId: Math.random().toString( 36 ) };
         const expectedAction = {
-            type: actions.TYPES.SET_ACTIVE_TAB,
-            payload
-        };
-        expect( actions.setActiveTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to closeTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.CLOSE_TAB,
-            payload
-        };
-        expect( actions.closeTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to reopenTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.REOPEN_TAB,
-            payload
-        };
-        expect( actions.reopenTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to updateTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.UPDATE_TAB,
+            type : actions.TYPES.UPDATE_TAB,
             payload
         };
         expect( actions.updateTab( payload ) ).toEqual( expectedAction );
+    } );
+    it( 'should set addressbar selected', () =>
+    {
+        const payload = { tabId: Math.random().toString( 36 ) };
+        const expectedAction = {
+            type : actions.TYPES.SELECT_ADDRESS_BAR,
+            payload
+        };
+        expect( actions.selectAddressBar( payload ) ).toEqual( expectedAction );
+    } );
+
+    it( 'should set addressbar deselected', () =>
+    {
+        const payload = { tabId: Math.random().toString( 36 ) };
+        const expectedAction = {
+            type : actions.TYPES.DESELECT_ADDRESS_BAR,
+            payload
+        };
+        expect( actions.deselectAddressBar( payload ) ).toEqual( expectedAction );
     } );
 } );

manavbp/safe_browser

__tests__/actions/tabs.spec.ts
@@ -1,52 +1,41 @@
 import * as actions from '$Actions/tabs_actions';
 
-describe( 'tab actions', () => {
-    it( 'should have types', () => {
-        expect( actions.TYPES ).toBeDefined();
-    } );
+describe('tab actions', () => {
+  it('should have types', () => {
+    expect(actions.TYPES).toBeDefined();
+  });
 
-    it( 'should create an action to add a tab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.ADD_TAB,
-            payload
-        };
-        expect( actions.addTab( payload ) ).toEqual( expectedAction );
-    } );
+  it('should create an action to add a tab', () => {
+    const payload = { url: 'hi', tabId: Math.random().toString(36) };
+    const expectedAction = {
+      type: actions.TYPES.ADD_TAB,
+      payload
+    };
+    expect(actions.addTab(payload)).toEqual(expectedAction);
+  });
+  it('should create an action to updateTab', () => {
+    const payload = { url: 'hi', tabId: Math.random().toString(36) };
+    const expectedAction = {
+      type: actions.TYPES.UPDATE_TAB,
+      payload
+    };
+    expect(actions.updateTab(payload)).toEqual(expectedAction);
+  });
+  it('should set addressbar selected', () => {
+    const payload = { tabId: Math.random().toString(36) };
+    const expectedAction = {
+      type: actions.TYPES.SELECT_ADDRESS_BAR,
+      payload
+    };
+    expect(actions.selectAddressBar(payload)).toEqual(expectedAction);
+  });
 
-    it( 'should create an action to setActiveTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.SET_ACTIVE_TAB,
-            payload
-        };
-        expect( actions.setActiveTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to closeTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.CLOSE_TAB,
-            payload
-        };
-        expect( actions.closeTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to reopenTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.REOPEN_TAB,
-            payload
-        };
-        expect( actions.reopenTab( payload ) ).toEqual( expectedAction );
-    } );
-
-    it( 'should create an action to updateTab', () => {
-        const payload = { url: 'hi' };
-        const expectedAction = {
-            type: actions.TYPES.UPDATE_TAB,
-            payload
-        };
-        expect( actions.updateTab( payload ) ).toEqual( expectedAction );
-    } );
-} );
+  it('should set addressbar deselected', () => {
+    const payload = { tabId: Math.random().toString(36) };
+    const expectedAction = {
+      type: actions.TYPES.DESELECT_ADDRESS_BAR,
+      payload
+    };
+    expect(actions.deselectAddressBar(payload)).toEqual(expectedAction);
+  });
+});